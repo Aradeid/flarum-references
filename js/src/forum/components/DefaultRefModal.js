b1b5c3c141a464245c5bd0d081382afdd13cd8e4
block-cat <iuliancernei@gmail.com>
2022-02-18 11:06:36 +0200

    implements

--- js/src/forum/components/DefaultRefModal.js
+++ js/src/forum/components/DefaultRefModal.js
@@ -6,7 +6,17 @@ export default class DefaultRefModal extends Modal {
   oninit(vnode) {
     super.oninit(vnode);
 
-    this.reference = Stream('');
+    this.value = app.composer.fields.content() || '';
+    this.allReferences = this.value.match(/\[reference\ id=[\w]*\][\w\ \(\)\[\]\.\,\/\\\-_\+;:"'?&]*\[\/reference\]/gm);
+    this.referenceId = this.allReferences?.length || 0;
+
+    // console.log(this.value);
+    // console.log(this.allReferences);
+    // console.log(this.referenceId);
+    this.fields = {
+      title: Stream(''),
+      link: Stream(''),
+    };
   }
 
   className() {
@@ -22,18 +32,19 @@ export default class DefaultRefModal extends Modal {
       m('.Modal-body',
         m('Form.Form--left',
         m('.Form-group',
-          m('label', app.translator.trans('Link')),
+          m('label', app.translator.trans('Titlul')),
           m('input.FormControl', {
             type: "text",
-            bidi: this.reference,
+            bidi: this.fields.title,
+            required: true,
           },
           )
         ),
         m('.Form-group',
-          m('label', app.translator.trans('Titlul')),
+          m('label', app.translator.trans('Link')),
           m('input.FormControl', {
-            type: "text",
-            bidi: this.reference,
+            type: "url",
+            bidi: this.fields.link,
           },
           )
         ),
@@ -50,13 +61,37 @@ export default class DefaultRefModal extends Modal {
   }
 
   addReference() {
-    app.composer.editor.insertAtCursor('[ref id=ref1]' + 1 + '[/ref]\ntesdfd', false);
-    app.composer.editor.insertAtCursor("\nadadedf fsdhfshdfkhsdkh kasdhfksdhfkasdhkf");
-    app.composer.editor.insertAtCursor('\nadadedf fsdhfshdfkhsdkh kasdhfksdhfkasdhkf\n');
-    app.composer.editor.insertAtCursor('\nadadedf fsdhfshdfkhsdkh kasdhfksdhfkasdhkf\n');
-    app.composer.editor.insertAtCursor('\nadadedf fsdhfshdfkhsdkh kasdhfksdhfkasdhkf\n');
-    app.composer.editor.insertAtCursor('\nReferinte:\n\n');
-    app.composer.editor.insertAtCursor('  1. [reference id=ref1]' + this.reference() + '[/reference]');
+    if (flarum.extensions['askvortsov-rich-text']) { // implements for askvortsov-rich-text editor
+      app.composer.editor.insertAtCursor("[ref id=ref" + ++this.referenceId + "]" + this.referenceId + "[/ref]");
+      app.composer.editor.moveCursorTo(app.composer.fields.content().length + 1);
+      app.composer.editor.insertAtCursor(" ");
+      
+      if (this.referenceId == 1) {
+        app.composer.editor.insertAtCursor(
+          "\nReferinte:\n" +
+          this.referenceId +
+          ". [reference id=ref" + this.referenceId + "] " +
+          this.fields.title() +
+          ". " +
+          this.fields.link() +
+          " [/reference] "
+          , false);
+      } else {
+        app.composer.editor.insertAtCursor(
+          "\n" +
+          this.referenceId +
+          ". [reference id=ref" + this.referenceId + "] " +
+          this.fields.title() +
+          ". " +
+          this.fields.link() +
+          " [/reference] "
+          , false);
+      }
+    } else { // implements for flarum-markdown editor
+      // app.composer.editor.insertAtCursor("[ref id=ref" + ++this.referenceId + "]" + this.referenceId + "[/ref]");
+      // app.composer.editor.moveCursorTo(app.composer.fields.content().length + 1);
+    }
+
     app.modal.close();
   }
 }
