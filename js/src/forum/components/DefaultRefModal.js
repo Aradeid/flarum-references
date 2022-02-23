089d2d167e6273368583d8865d1d5bb12bf06bf3
block-cat <iuliancernei@gmail.com>
2022-02-23 11:50:58 +0200

    adding reference for DefaultRef

--- js/src/forum/components/DefaultRefModal.js
+++ js/src/forum/components/DefaultRefModal.js
@@ -28,6 +28,8 @@ export default class DefaultRefModal extends Modal {
   }
 
   content() {
+    // console.log(app.composer.editor.attrs.menuState.editorView.dom.outerText);
+    // console.log(app.composer.editor.attrs.menuState.editorView.dom.outerText.length);
     return [
       m('.Modal-body',
         m('Form.Form--left',
@@ -61,34 +63,68 @@ export default class DefaultRefModal extends Modal {
   }
 
   addReference() {
+    // console.log(flarum.extensions['askvortsov-rich-text']);
     if (flarum.extensions['askvortsov-rich-text']) { // implements for askvortsov-rich-text editor
       app.composer.editor.insertAtCursor("[ref id=ref" + ++this.referenceId + "]" + this.referenceId + "[/ref]");
-      app.composer.editor.moveCursorTo(app.composer.fields.content().length + 1);
-      app.composer.editor.insertAtCursor(" ");
+
+      const position = app.composer.editor.getSelectionRange();
       
       if (this.referenceId == 1) {
-        app.composer.editor.insertAtCursor(
-          "\nReferinte:\n" +
-          this.referenceId +
-          ". [reference id=ref" + this.referenceId + "] " +
-          this.fields.title() +
-          ". " +
-          this.fields.link() +
-          " [/reference] "
-          , false);
+        app.composer.editor.moveCursorTo(app.composer.editor.attrs.menuState.editorView.dom.outerText.length + 1);
+        app.composer.editor.setEnter();
+        app.composer.editor.insertAtCursor("Referinte:");
+        app.composer.editor.setEnter();
       } else {
-        app.composer.editor.insertAtCursor(
-          "\n" +
-          this.referenceId +
-          ". [reference id=ref" + this.referenceId + "] " +
-          this.fields.title() +
-          ". " +
-          this.fields.link() +
-          " [/reference] "
-          , false);
+        app.composer.editor.moveCursorTo(app.composer.editor.attrs.menuState.editorView.dom.outerText.length + 1);
+        app.composer.editor.setEnter();
       }
+
+      app.composer.editor.insertAtCursor(
+        this.referenceId +
+        ". [reference id=ref" + this.referenceId + "] " +
+        this.fields.title() +
+        ". " +
+        this.fields.link() +
+        " [/reference]"
+      );
+
+      app.composer.editor.setSelectionRange(position[0], position[1]);
+      app.composer.editor.setEnter();
+      app.composer.editor.delete();
+
+      // this.array[0] = "Alt ceva"
+      // console.log("rich-text");
     } else { // implements for flarum-markdown editor
-      // app.composer.editor.insertAtCursor("[ref id=ref" + ++this.referenceId + "]" + this.referenceId + "[/ref]");
+      console.log(app.composer.editor.el.value);
+      console.log(app.composer.fields.content());
+      app.composer.editor.insertAtCursor("[ref id=ref" + ++this.referenceId + "]" + this.referenceId + "[/ref]");
+
+      const position = app.composer.editor.getSelectionRange();
+
+      if (this.referenceId == 1) {
+        app.composer.editor.moveCursorTo(app.composer.editor.el.value.length + 1);
+        // app.composer.editor.setEnter();
+        app.composer.editor.insertAtCursor("\n\nReferinte:\n");
+        // app.composer.editor.setEnter();
+      } else {
+        app.composer.editor.moveCursorTo(app.composer.editor.el.value.length + 1);
+        app.composer.editor.insertAtCursor("\n");
+        // app.composer.editor.setEnter();
+      }
+
+      app.composer.editor.insertAtCursor(
+        "\n" +
+        this.referenceId +
+        "\\. [reference id=ref" + this.referenceId + "] " +
+        this.fields.title() +
+        ". " +
+        this.fields.link() +
+        " [/reference]"
+      );
+
+      app.composer.editor.setSelectionRange(position[0], position[1]);
+
+      // console.log("non rich-text");
       // app.composer.editor.moveCursorTo(app.composer.fields.content().length + 1);
     }
 
