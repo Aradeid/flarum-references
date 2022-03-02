a1625070a83b489eb57292f388b3a5baa55d28a8
block-cat <iuliancernei@gmail.com>
2022-03-02 16:26:30 +0200

    test control link 2

--- extend.php
+++ extend.php
@@ -13,10 +13,10 @@ namespace Aradeid\FlarumReferences;
 
 use Flarum\Extend;
 use s9e\TextFormatter\Configurator;
-use BkC\Components\Extend\AddComponents;
+// use BkC\Components\Extend\AddComponents;
 
 return [
-    new AddComponents(),
+    // new AddComponents(),
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/resources/less/forum.less'),
